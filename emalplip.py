9920037b1bf7233378e72dced0e5f73e404653fd
Hookoored <hookoored4games@gmail.com>
2017-03-02 16:37:29 -0800

    Modified emalplip.py so when `python emalplip.py` is typed the shell will launch

--- emalplip.py
+++ emalplip.py
@@ -1,6 +1,9 @@
 import sys
 import run_code
 
-code_file = open(sys.argv[1])
-run_code.rc(code_file.read())
-code_file.close()
+if len(sys.argv) == 1:
+    execfile("EmShell.py")
+else:
+    code_file = open(sys.argv[1])
+    run_code.rc(code_file.read())
+    code_file.close()
