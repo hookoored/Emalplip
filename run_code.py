6c1b7da54812104c03b8941f3cdb72df635bfd83
Hookoogold <hookoored@users.noreply.github.com>
2017-03-27 16:19:25 -0700

    Fixed IndentationError

--- run_code.py
+++ run_code.py
@@ -1,9 +1,9 @@
 import functions as f
 def rc(code):
     for line in code:
-    if line[0] == "print":
-        for word in line[1:]:
-            print word
+        if line[0] == "print":
+            for word in line[1:]:
+                print word
 
-    else:
-        print line[0], "is not recognized as a function"
+        else:
+            print line[0], "is not recognized as a function"
