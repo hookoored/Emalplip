6a618846b51329be4466b9900bf78d4fbdb4edde
Hookoogold <hookoored@users.noreply.github.com>
2017-03-29 16:07:51 -0700

    Fixed a number of errors

--- emalplip.py
+++ emalplip.py
@@ -1,10 +1,11 @@
 from sys import argv
 from run_code import rc
 
-if len(sys.argv) == 1:
+if len(argv) == 1:
     execfile("shell.py")
 
 else:
     code_file = open(argv[1])
-    code = code_file.read().split('\n').split(' ')
-    rc(code)
+    code = code_file.read()
+    print type(code)
+    rc(code.split('\n'))
--- run_code.py
+++ run_code.py
@@ -1,9 +1,10 @@
 import functions as f
 def rc(code):
     for line in code:
-        if line[0] == "print":
-            for word in line[1:]:
+        split_line = line.split(' ')
+        if split_line[0] == "print":
+            for word in split_line[1:]:
                 print word
 
         else:
-            print line[0], "is not recognized as a function"
+            print split_line[0], "is not recognized as a function"
