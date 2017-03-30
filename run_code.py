eb8a434dd57d1b7fcf3e46e583c16c648f57d66c
Hookoogold <hookoored@users.noreply.github.com>
2017-03-29 17:53:01 -0700

    Added continue if the line is empty or only contains whitespace
    Basically, added continue if the line matches regular expressions `^$` or `^\s+$`

--- run_code.py
+++ run_code.py
@@ -1,10 +1,17 @@
 import functions as f
+import re
+
 def rc(code):
+    empty = re.compile("^$")
+    only_spaces = re.compile("^\s+$")
     for line in code:
         split_line = line.split(' ')
-        if split_line[0] == "print":
+        if empty.match(line) or only_spaces.match(line):
+            continue
+            
+        elif split_line[0] == "print":
             for word in split_line[1:]:
                 print word
-
+                
         else:
             print split_line[0], "is not recognized as a function"
